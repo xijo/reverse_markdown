101a177f4c6df0b528930cb490bcd44d0800ba2d
Johannes Opper <xijo@gmx.de>
2014-04-01 21:33:06 +0200

    Added specs for blockquote entrails, see #16

---
+++ spec/lib/reverse_markdown/converters/blockquote_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe ReverseMarkdown::Converters::Blockquote do
+
+  let(:converter) { ReverseMarkdown::Converters::Blockquote.new }
+
+  it 'converts nested elements as well' do
+    input = Nokogiri::XML.parse("<blockquote><ul><li>foo</li></ul></blockquote>").root
+    result = converter.convert(input)
+    result.should eq "> - foo"
+  end
+
+  it 'can deal with paragraphs inside' do
+    input = Nokogiri::XML.parse("<blockquote><p>Some text.</p><p>Some more text.</p></blockquote>").root
+    result = converter.convert(input)
+    result.should eq "> Some text.\n> \n> Some more text."
+  end
+end
