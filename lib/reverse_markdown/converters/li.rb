0c248d9a03bbc5cb4cf31dfeeffdcc978db92474
Johannes Opper <xijo@gmx.de>
2014-04-02 15:35:25 +0200

    Ensure that li converter can handle non-parent-context, see #28

--- lib/reverse_markdown/converters/li.rb
+++ lib/reverse_markdown/converters/li.rb
@@ -19,7 +19,7 @@ module ReverseMarkdown
 
       def indentation_for(node)
         length = node.ancestors('ol').size + node.ancestors('ul').size
-        '  ' * (length - 1)
+        '  ' * [length - 1, 0].max
       end
     end
 
---
+++ spec/lib/reverse_markdown/converters/li_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe ReverseMarkdown::Converters::Li do
+
+  let(:converter) { ReverseMarkdown::Converters::Li.new }
+
+  it 'does not fail without a valid parent context' do
+    input = Nokogiri::XML.parse("<li>foo</li>").root
+    result = converter.convert(input)
+    result.should eq "- foo\n"
+  end
+
+end
