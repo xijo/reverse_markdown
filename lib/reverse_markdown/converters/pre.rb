864b43a2470a1947d07ee718e4a482ba7cd98641
Benjamin Dobell <benjamin.dobell+git@glassechidna.com.au>
2014-10-15 05:23:52 +1100

    Preserve Github Flavor Markdown code block language information

--- lib/reverse_markdown/converters/pre.rb
+++ lib/reverse_markdown/converters/pre.rb
@@ -5,7 +5,9 @@ module ReverseMarkdown
     class Pre < Base
       def convert(node)
         if ReverseMarkdown.config.github_flavored
-          "```\n" << node.text.strip << "\n```\n"
+          highlight_match = node.parent['class'].nil? ? nil : node.parent['class'].match(/highlight highlight-([a-zA-Z0-9]+)/)
+          language_string = highlight_match ? " #{highlight_match[1]}" : ''
+          "```#{language_string}\n" << node.text.strip << "\n```\n"
         else
           "\n\n    " << node.text.strip.lines.to_a.join("    ") << "\n\n"
         end
