de0d042427f0bf5f25d71cb2592e220c6a8e5a90
Goran Miskovic <schkovich@gmail.com>
2015-01-29 02:40:46 +0100

    Pre class testing: - case when no syntax_highlight option is given - case when no syntax_highlight option dobell is given - case when no syntax_highlight option confluence  is given

--- spec/lib/reverse_markdown/converters/pre_spec.rb
+++ spec/lib/reverse_markdown/converters/pre_spec.rb
@@ -8,21 +8,41 @@ describe ReverseMarkdown::Converters::Pre do
       expect(converter.convert(node)).to include " puts foo\n"
     end
   end
-  context 'for github_flavored markdown' do
+  context 'for github_flavored markdown plain' do
     before { ReverseMarkdown.config.github_flavored = true }
     it 'converts with backticks' do
       node = node_for("<pre>puts foo</pre>")
       expect(converter.convert(node)).to include "```\nputs foo\n```"
     end
-    before { ReverseMarkdown.config.syntax_highlight = 'dobell' }
+    it 'includes the given highlight language' do
+      node = node_for("<div class='highlight highlight-ruby'><pre>puts foo</pre></div>")
+      expect(converter.convert(node.children.first)).to include "```\nputs foo\n```"
+    end
+    it 'includes the given highlight language confluence way' do
+      node = node_for("<pre class=\"theme: Confluence; brush: php; gutter: false\">echo 'foo'</pre>")
+      expect(converter.convert(node)).to include "```\necho 'foo'\n```"
+    end
+  end
+  context 'for github_flavored markdown dobell' do
+    before { ReverseMarkdown.config.github_flavored = true, ReverseMarkdown.config.syntax_highlight = 'dobell' }
+    it 'converts with backticks' do
+      node = node_for("<pre>puts foo</pre>")
+      expect(converter.convert(node)).to include "```\nputs foo\n```"
+    end
     it 'includes the given highlight language' do
       node = node_for("<div class='highlight highlight-ruby'><pre>puts foo</pre></div>")
       expect(converter.convert(node.children.first)).to include "```ruby\n"
     end
-    before { ReverseMarkdown.config.syntax_highlight = 'confluence' }
+  end
+  context 'for github_flavored markdown confluence' do
+    before { ReverseMarkdown.config.github_flavored = true, ReverseMarkdown.config.syntax_highlight = 'confluence' }
+    it 'converts with backticks' do
+      node = node_for("<pre>puts foo</pre>")
+      expect(converter.convert(node)).to include "```\nputs foo\n```"
+    end
     it 'includes the given highlight language confluence way' do
       node = node_for("<pre class=\"theme: Confluence; brush: php; gutter: false\">echo 'foo'</pre>")
-      expect(converter.convert(node.children.first)).to include "```php\n"
+      expect(converter.convert(node)).to include "```php\n"
     end
   end
 end
