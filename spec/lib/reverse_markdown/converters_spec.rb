6866c8b90e9cd3d3636d09f1217cca4f16b0fa25
Johannes Opper <johannes.opper@gmail.com>
2015-11-13 12:51:27 +0100

    Remove some rspec deprecation warnings

--- spec/lib/reverse_markdown/converters_spec.rb
+++ spec/lib/reverse_markdown/converters_spec.rb
@@ -6,13 +6,13 @@ describe ReverseMarkdown::Converters do
 
   describe '.register and .unregister' do
     it 'adds a converter mapping to the list' do
-      expect { converters.lookup(:foo) }.to raise_error
+      expect { converters.lookup(:foo) }.to raise_error ReverseMarkdown::UnknownTagError
 
       converters.register :foo, :foobar
       expect(converters.lookup(:foo)).to eq :foobar
 
       converters.unregister :foo
-      expect { converters.lookup(:foo) }.to raise_error
+      expect { converters.lookup(:foo) }.to raise_error ReverseMarkdown::UnknownTagError
     end
   end
 
