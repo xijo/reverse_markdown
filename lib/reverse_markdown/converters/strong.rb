562e0a568f8b71d6ebd7906400db9adfc1416168
Usman Asif <usman@fivetonine.co>
2020-07-30 22:43:17 -0700

    Slack Flavoured Strong Format

--- lib/reverse_markdown/converters/strong.rb
+++ lib/reverse_markdown/converters/strong.rb
@@ -6,7 +6,7 @@ module ReverseMarkdown
         if content.strip.empty? || state[:already_strong]
           content
         else
-          "#{content[/^\s*/]}**#{content.strip}**#{content[/\s*$/]}"
+          "#{content[/^\s*/]}*#{content.strip}*#{content[/\s*$/]}"
         end
       end
     end
